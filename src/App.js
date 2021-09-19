9cf16deeb9c3e8205443ec3aadfa0d08eefdf49a
UzairSammad <uzair.iu17@gmail.com>
2021-09-19 11:29:42 +0500

    file_uplaod_example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [imageUrl, setImageURL] = useState(null)
+  const [imageFile, setImageFile] = useState(null)
+  const handleImageChange = (e) => {
+    setImageFile(e.target.files[0])
+    setImageURL(URL.createObjectURL(e.target.files[0]))
+
+  }
+  const handleSubmit = () => {
+    const source = {
+      uri: '../assets/images/graphic1.webp',
+      type: imageFile.type,
+      namee: imageFile.name
+    }
+    let formData = new FormData()
+    formData.append('imageFile',source)
+    console.log(formData, 'sourcesource')
+
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={imageUrl} style={{ width: '250px', height: '250px' }} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Upload Image Example
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type='file' placeholder='Upload Image' onChange={e => handleImageChange(e)} />
+        <input type='submit' placeholder='Submit' style={{ marginTop: '20px', fontSize: '16px' }} onClick={handleSubmit} />
+
       </header>
     </div>
   );
